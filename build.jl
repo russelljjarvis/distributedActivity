07f76a755d38d2f482eca22904caf7e71099a93a
Russell Jarvis <rjarvis@westernsydney.edu.au>
2023-01-16 10:18:15 +1100

    adding build instructions to staging area

---
+++ build.jl
@@ -0,0 +1,11 @@
+using Pkg
+Pkg.add("Distributions")
+Pkg.add("OhMyREPL")
+Pkg.add("DelimitedFiles")
+Pkg.add("Plots")
+Pkg.add("SparseArrays")
+Pkg.add("LinearAlgebra")
+Pkg.add("Revise")
+Pkg.add("Random")
+
+
